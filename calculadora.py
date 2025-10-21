6d083060b229cca98399d5254199a07b8c80eef4
Mihai Taran <mihai.taran@WIRTZ.LOCAL>
2025-10-21 20:02:38 +0200

    usuario1: merge de feature_suma a develop

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,23 @@
-def calculadora(a,b):
-    print("Bienvenido a mi calculadora:")
+import suma
+
+def calculadora():
+    
+    while True:
+        n = int(input("""Introduzca qué desea hacer:
+1 - Sumar
+2 - Salir
+"""))
+
+        if n == 2:
+            print("Saliendo...")
+            break
+
+        a = int(input("Introduzca el primer número: "))
+        b = int(input("Introduzca el segundo número: "))
+
+        if n == 1:
+            print("El resultado es", suma.suma(a, b))
+        else:
+            print("Opción no válida")
+
+calculadora()
